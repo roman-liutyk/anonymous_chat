be6962df6424939534a01599649c4cfab83ac0c9
roman-liutyk <lytikroman5@gmail.com>
2024-04-27 13:31:30 +0200

    Added user models

---
+++ api/models/user_model.dart
@@ -0,0 +1,13 @@
+class UserModel {
+  const UserModel({
+    required this.id,
+    required this.username,
+    required this.email,
+    required this.password,
+  });
+
+  final String id;
+  final String username;
+  final String email;
+  final String password;
+}
